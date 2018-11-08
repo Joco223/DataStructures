943767e582ba73d90cc39e36063d50f0cb9b4b79
Joco223 <jovanmihajlovic22@gmail.com>
2018-11-08 19:46:33 +0100

    More LinkedList functions

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -12,6 +12,12 @@ private:
 	int length;
 public:
 
+	//Constructor without parameters
+	LinkedList() {
+		first = nullptr;
+		length = 0;
+	}
+
 	//Constructor, create a linked list with given elements
 	LinkedList(std::initializer_list<T> input) {
 		first = nullptr;
@@ -25,35 +31,66 @@ public:
 		}	
 	};
 
-	//Add a new element at the target position to the linked list
+	//Get pointer to first element
+	Link* getFirst() {return first;}
+
+	//Get the length of the linked list
+	int getLength() {return length;}
+
+	//Add a new element after the target position to the linked list
 	void addElement(T element, int targetPos) {
-		int id = 0;
+		int i = 0;
 		Link* target = first;
-		while(id != targetPos-2) {
+		Link* prev = nullptr;
+		while(i < targetPos) {
+			if(prev == nullptr) {prev = first;}else{prev = prev->next;}
 			target = target->next;
-			id++;
+			i++;
 		}
 		Link* current = new Link(element, nullptr);
 		current->content = element;
-		current->next = target->next;
-		target->next = current;
+		current->next = target;
+		if(prev != nullptr) {prev->next = current;}
+		if(i == 0) {first = current;}
 		length++;
 	}
 
 	//Remove an element from the target position from the linked list
 	void removeElement(int targetPos) {
-		int id = 0;
+		int i = 0;
 		Link* target = first;
 		Link* prev = nullptr;
-		while(id != targetPos-1) {
-			prev = target;
+		while(i < targetPos) {
+			if(prev == nullptr) {prev = first;}else{prev = prev->next;}
 			target = target->next;
-			id++;
+			i++;
 		}
-		prev->next = target->next;
+		if(i == 0) {first = first->next;}else{prev->next = target->next;}
 	}
 
-	//Print the contents of all elements of the linked list
+	//Apply a function to all elements of the linked list
+	void applyFunction(T f(T)) {
+		Link* current;
+		while(current != nullptr) {
+			current->content = f(current->content);
+			current = current->next;
+		}
+	}
+
+	//Apply a function to pairs of elements from two linked lists
+	static LinkedList<T> applyFunctionToPairs(LinkedList<T> A, LinkedList<T> B, T f(T, T)) {
+		LinkedList<T> newLL;
+		Link* currentA = A.first;
+		Link* currentB = B.first;
+		while(currentA != nullptr || currentB != nullptr) {
+			newLL.addElement(f(currentA->content, currentB->content), newLL.getLength());
+			currentA = currentA->next;
+			currentB = currentB->next;
+		}
+		return newLL;
+	}
+
+	//Print all of the elements of the linked list
 	void print() {
 		Link* current = first;
 		while(current != nullptr) {
--- source/Main.cpp
+++ source/Main.cpp
@@ -2,12 +2,18 @@
 
 #include "LinkedList.h"
 
+int biggerThan10(int x) {return x>10;}
+int square(int x) {return x*x;}
+int addTen(int x) {return x+10;}
+
+int addPairs(int x, int y) {return x + y;}
+
 int main(int argc, char** argv) {
 
-	LinkedList<int> testLL = {5, 11, 345, 999, 1010101};
-	testLL.addElement(525, 3);
-	testLL.removeElement(3);
-	testLL.print();
+	LinkedList<int> A = {12, 55, 1, 88, -10};
+	LinkedList<int> B = {5 , 11, 5, 2 ,  20};
+	LinkedList<int> C = LinkedList<int>::applyFunctionToPairs(A, B, addPairs);
+	C.print();
 
 	return 0;
 }
