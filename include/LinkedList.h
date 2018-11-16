7718eb830982b6a1ebaf5b805ac4b85be78c78c3
Joco223 <jovanmihajlovic22@gmail.com>
2018-11-16 23:14:56 +0100

    Bug fixes

--- include/LinkedList.h
+++ include/LinkedList.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 namespace jci {
 	template<class T>
--- source/Main.cpp
+++ source/Main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <chrono>
+#include <string>
 
 #include "LinkedList.h"
 #include "Stack.h"
@@ -24,42 +25,38 @@ bool same(int x, int y) {return x == y;}
 
 int main(int argc, char** argv) {
 
-	/* jci::Array Tests compared to std::vector:
-	Add elements to front - (Standard first: +68% - Custom First: +57%)
-	*/
-
 	int loopCount = 50;
-	int sampleSize = 40000;
+	int sampleSize = 1000000;
 
 	std::chrono::high_resolution_clock::time_point t3 = std::chrono::high_resolution_clock::now();
 	for(int i = 0; i < loopCount; i++) {
-		std::vector<int> standard;
-		for(int j = 0; j < sampleSize; j++) {standard.insert(standard.begin(), j);}
-		for(int j = 0; j < sampleSize; j++) {standard.erase(standard.begin());}
-		std::cout << "Standard implementation round: " << i << " finished." << '\r';
+		std::list<int> standard;
+		for(int j = 0; j < sampleSize; j++) {standard.push_back(j);}
+		for(auto& i : standard) {i = square(i);}
+		std::cerr << "Standard implementation round: " << i << " finished." << '\r';
 	}
 	std::chrono::high_resolution_clock::time_point t4 = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> timeSpan2 = std::chrono::duration_cast<std::chrono::duration<double>>(t4 - t3);
-	std::cout << "Standard implementation average took: " << timeSpan2.count()/loopCount << " seconds." << '\n';
+	std::cerr << "Standard implementation average took: " << timeSpan2.count()/loopCount << " seconds." << '\n';
 
-	/*std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+	std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 	for(int i = 0; i < loopCount; i++) {
-		jci::Array<int> mine;
-		for(int j = 0; j < sampleSize; j++) {mine.addElementFront(j);}
-		for(int j = 0; j < sampleSize; j++) {mine.removeFrontElement();}
-		std::cout << "Custom implementation round: " << i << " finished." << '\r';
+		jci::LinkedList<int> mine;
+		for(int j = 0; j < sampleSize; j++) {mine.addElementBack(j);}
+		mine.applyFunction(square);
+		std::cerr << "Custom implementation round: " << i << " finished." << '\r';
 	}
 	std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
 	std::chrono::duration<double> timeSpan = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
-	std::cout << "Custom implementation average took: " << timeSpan.count()/loopCount << " seconds." << '\n';*/
+	std::cerr << "Custom implementation average took: " << timeSpan.count()/loopCount << " seconds." << '\n';
 
-	
+	std::cerr.precision(2);
 
-	/*int amount = timeSpan2.count()/timeSpan.count()*100 - 100;
+	float amount = timeSpan2.count()/timeSpan.count()*100 - 100;
 	
-	std::cout << "Custom implementation speed is: ";
-	if(amount < 0) {std::cout << "-";}else{std::cout << "+";};
-	std::cout << amount << "% standard implementation speed." << '\n';*/
+	std::cerr << "Custom implementation speed is: ";
+	if(amount > 0) {std::cerr << "+";}
+	std::cerr << amount << "% standard implementation speed." << '\n';
 
 	return 0;
 }
